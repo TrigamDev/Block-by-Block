15eeb6d6624d53e4dbd88a61441c3547145b82ec
TrigamDev <trigambusiness@gmail.com>
2025-08-01 22:07:32 -0400

    Init

---
+++ src/main/java/dev/trigam/modules/BlockByBlock.java
@@ -0,0 +1,19 @@
+package dev.trigam.modules;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BlockByBlock implements ModInitializer {
+	
+	public static final String MOD_ID = "block-by-block";
+	public static final Logger LOGGER = LoggerFactory.getLogger( MOD_ID );
+
+	@Override
+	public void onInitialize() {
+	
+	
+	
+	}
+}
